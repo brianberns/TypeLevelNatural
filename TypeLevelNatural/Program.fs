2f498416306d8d9ec2ca159d3fb3275e20388f72
Brian Berns <brianberns@gmail.com>
2023-11-21 10:45:20 -0500

    Add project files.

---
+++ TypeLevelNatural/Program.fs
@@ -0,0 +1,6 @@
+﻿namespace TypeLevelNatural
+
+module Program =
+
+    let v = Vector.zeroCreate<int, Nat1200>
+    printfn "%A" v
---
+++ TypeLevelNatural/Vector.fs
@@ -0,0 +1,17 @@
+﻿namespace TypeLevelNatural
+
+type Vector<'t, 'nCols when 'nCols :> Natural> =
+    private MkVector of 't[]
+
+module Vector =
+
+    let zeroCreate<'t, 'nCols when 'nCols :> Natural> : Vector<'t, 'nCols> =
+        Array.zeroCreate<'t> 'nCols.Size
+            |> MkVector
+
+    let init<'t, 'nCols when 'nCols :> Natural> initializer : Vector<'t, 'nCols> =
+        Array.init<'t> 'nCols.Size initializer
+            |> MkVector
+
+type Matrix<'t, 'nRows, 'nCols when 'nRows :> Natural and 'nCols :> Natural> =
+    private MkMatrix of 't[,]
